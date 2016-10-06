8a66939d7331ff143b2b76b4da67d8af11f95abc
Gerardo Guzman <jerryguzman873@gmail.com>
2016-10-05 18:00:27 -0700

    added a car and road

--- drawing.js
+++ drawing.js
@@ -12,8 +12,36 @@ canvas.beginPath();
 canvas.arc(100, 75, 50, 0, 2*Math.PI);
 canvas.stroke();
 
-canvas.fillStyle = "pink";
-canvas.fillRect(80, 150, 100, 100);
 
-canvas.fillStyle = "green";
-canvas.fillRect(80, 150, 100, 100);
+//background
+canvas.fillStyle = "black";
+canvas.fillRect(0,556,1050,150);
+
+//lines
+canvas.fillStyle = "yellow";
+canvas.fillRect(0,600,90,50);
+
+canvas.fillStyle = "yellow";
+canvas.fillRect(200,600,200,50);
+
+canvas.fillStyle = "yellow";
+canvas.fillRect(500,600,200,50);
+
+//tire
+canvas.beginPath();
+canvas.fillStyle = "black";
+canvas.arc(100,500,70,0,2*Math.PI,true);
+canvas.fill();
+
+canvas.beginPath();
+canvas.fillStyle = "black";
+canvas.arc(530,500,70,0,2*Math.PI,true);
+canvas.fill();
+
+//body
+canvas.fillStyle = "red";
+canvas.fillRect(0,290,1000,150);
+
+//top of the car
+canvas.fillStyle = "red";
+canvas.fillRect(0,70,750,220);
